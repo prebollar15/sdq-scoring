4fdbcf2a5514952c5a6698dcb6b57f4c97a2cfa8
Pedro Rebollar <prebolla15@gmail.com>
2022-12-02 00:15:13 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('You have submitted the form.')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      <form onSubmit={handleSubmit}>
+      <fieldset>
+         <label>
+           <p>Name</p>
+           <input name="name" />
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
