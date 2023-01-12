e9e93a18eae9bb077289c4cc1f6b623de27c67e0
Pedro Rebollar <prebolla15@gmail.com>
2023-01-12 13:00:30 -0600

    updated with App and Form components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 function App() {
-  const handleSubmit = event => {
-    event.preventDefault();
-    alert('You have submitted the form.')
-  }
 
   return (
-    <div className="wrapper">
-      <h1>How About Them Apples</h1>
-      <form onSubmit={handleSubmit}>
-      <fieldset>
-         <label>
-           <p>Name</p>
-           <input name="name" />
-         </label>
-       </fieldset>
-       <button type="submit">Submit</button>
-      </form>
-    </div>
+    <> <Form />
+    </>
   );
 }
 
